88b66b296c85ef3c1e0d0f100480ae9a9e7d7f02
hayoon <hayoon0524@konkuk.ac.kr>
2022-07-04 17:48:37 +0900

    api change

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
-export const api2 =
-    "http://dev-api-team3.openinfra-kr.org";
 export const api =
+    "http://dev-api-team3.openinfra-kr.org";
+export const api2 =
     "http://localhost:8000";
