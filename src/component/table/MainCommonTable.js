537edc2031d96e2e3a47a45f7cd71b1ff6ac58d3
hayoon <hayoon0524@konkuk.ac.kr>
2022-07-11 18:57:53 +0900

    paging

--- src/component/table/MainCommonTable.js
+++ src/component/table/MainCommonTable.js
@@ -9,25 +9,24 @@ import styles from "../../css/Component.module.css";
 import {api, api2} from "../../Config";
 
 
-
 const MainCommonTable = props => {
     const [datalist, setDatalist] = useState([]);
+    const [pageNum, setPageNum] = useState(1);
     const [url, setURL] = useState([]);
-    let urllist =new Array();
+    let urllist = new Array();
     let user = localStorage.getItem('id') || '';
-
 
     useEffect(() => {
         const page = '1';
+
         async function getBoardInfo() {
 
             try {
-                const response = await axios.get(api + '/boards/' +page, {
-                }).then(res => {
+                const response = await axios.get(api + '/boards/' + page, {}).then(res => {
                     console.log(res.data);
                     setDatalist(res.data);
-                    res.data.map((row,index)=>{
-                        url.push('boards/one/'+row.bid);
+                    res.data.map((row, index) => {
+                        url.push('boards/one/' + row.bid);
                         console.log(url[index]);
                     })
                 });
@@ -36,28 +35,44 @@ const MainCommonTable = props => {
                 console.error(error);
             }
         }
+
         getBoardInfo();
-    },[]);
+    }, [pageNum]);
 
+    const decreasePage = (e) => {
+        if (pageNum > 1)
+            setPageNum(pageNum => pageNum -1)
+    }
+
+    const increasePage = (e) => {
+        setPageNum(pageNum => pageNum + 1)
+    }
     return (
-        <CommonTable headersName={['글번호', '아이디', '제목', '등록일']} pos={props.pos}>
-            {
+        <div>
+            <CommonTable headersName={['글번호', '아이디', '제목', '등록일']} pos={props.pos}>
+                {
 
-            datalist.map((row,index) => (
-                <CommonTableRow>
-                    {/*{setURL('boards/one/' + row.bid)}*/}
-                    <CommonTableColumn>{row.bid}</CommonTableColumn>
-                    <CommonTableColumn>{row.u_id}</CommonTableColumn>
-                    <CommonTableColumn>{row.title}</CommonTableColumn>
-                    <CommonTableColumn>{row.enter_date}</CommonTableColumn>
-                    <button onClick = { () => {
-                        window.location.href = url[index]
-                    }}>게시물 보기</button>
-                </CommonTableRow>
-            ))
-            }
+                    datalist.map((row, index) => (
+                        <CommonTableRow>
+                            {/*{setURL('boards/one/' + row.bid)}*/}
+                            <CommonTableColumn>{row.bid}</CommonTableColumn>
+                            <CommonTableColumn>{row.u_id}</CommonTableColumn>
+                            <CommonTableColumn>{row.title}</CommonTableColumn>
+                            <CommonTableColumn>{row.enter_date}</CommonTableColumn>
+                            <button onClick={() => {
+                                window.location.href = url[index]
+                            }}>게시물 보기
+                            </button>
+                        </CommonTableRow>
+                    ))
+                }
 
-        </CommonTable>
+            </CommonTable>
+            <div className={styles.divButton}>
+                <button className={styles.btn} onClick={decreasePage}>왼쪽 페이지 가기 </button>
+                <button className={styles.btn} onClick={increasePage}>오른쪽 페이지 가기 </button>
+            </div>
+        </div>
     )
 }
 
